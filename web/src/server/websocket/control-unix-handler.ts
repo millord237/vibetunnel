4fff4ce6c608d12982e2de368029c3793ce17c47
Rajiv <millord237@gmail.com>
2025-07-21 15:34:44 -0400

    Revert "fix: Set correct cwd for spawned terminal processes"
    This reverts commit 46b592ccdadeb96936052790f348039023c3c939.

--- web/src/server/websocket/control-unix-handler.ts
+++ web/src/server/websocket/control-unix-handler.ts
@@ -19,8 +19,6 @@ interface MessageHandler {
 }
 
 class TerminalHandler implements MessageHandler {
-  constructor(private controlUnixHandler: ControlUnixHandler) {}
-
   async handleMessage(message: ControlMessage): Promise<ControlMessage> {
     logger.log(`Terminal handler: ${message.action}`);
 
@@ -48,16 +46,10 @@ class TerminalHandler implements MessageHandler {
         // Execute vibetunnel command
         logger.log(`Spawning terminal with args: ${args.join(' ')}`);
 
-        // Set CWD to the web directory to ensure node-pty can be found
-        const repoPath = this.controlUnixHandler.getRepositoryPath();
-        const webPath = repoPath ? path.join(repoPath, 'web') : undefined;
-        logger.log(`Using web path for cwd: ${webPath}`);
-
         // Use spawn to avoid shell injection
         const vt = child_process.spawn('vibetunnel', args, {
           detached: true,
           stdio: 'ignore',
-          cwd: webPath,
         });
 
         vt.unref();
@@ -110,7 +102,6 @@ export class ControlUnixHandler {
   private readonly socketPath: string;
   private handlers = new Map<ControlCategory, MessageHandler>();
   private messageBuffer = Buffer.alloc(0);
-  private currentRepositoryPath: string | null = null;
 
   constructor() {
     // Use a unique socket path in user's home directory to avoid /tmp issues
@@ -127,7 +118,7 @@ export class ControlUnixHandler {
     this.socketPath = path.join(socketDir, 'control.sock');
 
     // Initialize handlers
-    this.handlers.set('terminal', new TerminalHandler(this));
+    this.handlers.set('terminal', new TerminalHandler());
     this.handlers.set('system', new SystemHandler(this));
   }
 
@@ -546,21 +537,6 @@ export class ControlUnixHandler {
       this.macSocket?.destroy();
       this.macSocket = null;
     }
-  }
-
-  /**
-   * Get the current repository path
-   */
-  getRepositoryPath(): string | null {
-    // This is a temporary solution. In the future, this should be
-    // dynamically determined or configured.
-    if (this.currentRepositoryPath) {
-      return this.currentRepositoryPath;
-    }
-    // A sensible default for development
-    const repoPath = path.resolve(process.cwd(), '../../');
-    this.currentRepositoryPath = repoPath;
-    return repoPath;
   }
 }
 
